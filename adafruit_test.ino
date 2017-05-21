6ca5c8f41e43536cf995b37abf2e87df21db4c7a
Alex Piechowski <alex@piechowski.org>
2017-05-21 16:16:09 -0500

    Initial commit (unable to build)
    libmegaADK_Adafruit_RA8875.a(Adafruit_RA8875.cpp.obj): In function
    `Adafruit_RA8875::Adafruit_RA8875(unsigned char, unsigned char)':
    /home/alex/Code/adafruit-test/libraries/Adafruit_RA8875/Adafruit_RA8875.cpp:667:
    undefined reference to `Adafruit_GFX::Adafruit_GFX(int, int)'
    libmegaADK_Adafruit_RA8875.a(Adafruit_RA8875.cpp.obj):(.rodata._ZTV15Adafruit_RA8875[vtable
    for Adafruit_RA8875]+0xa): undefined reference to
    `Adafruit_GFX::startWrite()'
    libmegaADK_Adafruit_RA8875.a(Adafruit_RA8875.cpp.obj):(.rodata._ZTV15Adafruit_RA8875[vtable
    for Adafruit_RA8875]+0xc): undefined reference to
    `Adafruit_GFX::writePixel(int, int, unsigned int)'
    libmegaADK_Adafruit_RA8875.a(Adafruit_RA8875.cpp.obj):(.rodata._ZTV15Adafruit_RA8875[vtable
    for Adafruit_RA8875]+0xe): undefined reference to
    `Adafruit_GFX::writeFillRect(int, int, int, int, unsigned int)'
    libmegaADK_Adafruit_RA8875.a(Adafruit_RA8875.cpp.obj):(.rodata._ZTV15Adafruit_RA8875[vtable
    for Adafruit_RA8875]+0x10): undefined reference to
    `Adafruit_GFX::writeFastVLine(int, int, int, unsigned int)'
    libmegaADK_Adafruit_RA8875.a(Adafruit_RA8875.cpp.obj):(.rodata._ZTV15Adafruit_RA8875[vtable
    for Adafruit_RA8875]+0x12): undefined reference to
    `Adafruit_GFX::writeFastHLine(int, int, int, unsigned int)'
    libmegaADK_Adafruit_RA8875.a(Adafruit_RA8875.cpp.obj):(.rodata._ZTV15Adafruit_RA8875[vtable
    for Adafruit_RA8875]+0x14): undefined reference to
    `Adafruit_GFX::writeLine(int, int, int, int, unsigned int)'
    libmegaADK_Adafruit_RA8875.a(Adafruit_RA8875.cpp.obj):(.rodata._ZTV15Adafruit_RA8875[vtable
    for Adafruit_RA8875]+0x16): undefined reference to
    `Adafruit_GFX::endWrite()'
    libmegaADK_Adafruit_RA8875.a(Adafruit_RA8875.cpp.obj):(.rodata._ZTV15Adafruit_RA8875[vtable
    for Adafruit_RA8875]+0x18): undefined reference to
    `Adafruit_GFX::setRotation(unsigned char)'
    libmegaADK_Adafruit_RA8875.a(Adafruit_RA8875.cpp.obj):(.rodata._ZTV15Adafruit_RA8875[vtable
    for Adafruit_RA8875]+0x1a): undefined reference to
    `Adafruit_GFX::invertDisplay(bool)'
    collect2: error: ld returned 1 exit status
    CMakeFiles/adafruit_test.dir/build.make:106: recipe for target
    'adafruit_test.elf' failed
    make[2]: *** [adafruit_test.elf] Error 1
    CMakeFiles/Makefile2:220: recipe for target
    'CMakeFiles/adafruit_test.dir/all' failed
    make[1]: *** [CMakeFiles/adafruit_test.dir/all] Error 2
    Makefile:83: recipe for target 'all' failed
    make: *** [all] Error 2

---
+++ adafruit_test.ino
@@ -0,0 +1,21 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_RA8875.h>
+#include <SPI.h>
+
+#define RA8875_INT 3
+#define RA8875_CS 10
+#define RA8875_RESET 9
+
+Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS, RA8875_RESET);
+
+void setup() {
+    tft.begin(RA8875_800x480);
+    tft.displayOn(true);
+    tft.GPIOX(true);      // Enable TFT - display enable tied to GPIOX
+    tft.PWM1config(true, RA8875_PWM_CLK_DIV1024); // PWM output for backlight
+    tft.PWM1out(255);
+}
+
+void loop() {
+    tft.fillRect(100, 100, 100, 100, RA8875_BLUE);
+}
